1da3fa63a41117aeabfdf554e210538afd3cc1c2
Brandon Valosek <bvalosek@gmail.com>
2016-01-28 20:07:22 -0600

    factoring out more to base twister device

--- bvalosek_mf_twister/Twister16ParamDevice.py
+++ bvalosek_mf_twister/Twister16ParamDevice.py
@@ -1,7 +1,6 @@
-from _Framework.DeviceComponent import DeviceComponent
+from TwisterDevice import TwisterDevice
 from _Framework.SubjectSlot import subject_slot_group
 from _Framework.SubjectSlot import subject_slot
-from math import ceil
 
 ON_COLOR = 45
 OFF_COLOR = 85
@@ -9,26 +8,18 @@ BANK_COLOR = 1
 BANK_ACTIVE_COLOR = 65
 BANK_NA_COLOR = 20
 
-class Twister16ParamDevice(DeviceComponent):
+class Twister16ParamDevice(TwisterDevice):
     """Control 16 parameters at a time on the twister, using the buttons to
     page through the banks
     """
 
     def __init__(self, *a, **k):
-        DeviceComponent.__init__(self, *a, **k)
-        self._param_buttons = []
+        TwisterDevice.__init__(self, *a, **k)
         self.active_bank = 0
-
-    def set_parameter_buttons(self, buttons):
-        """Set the ButtonMatrix of push buttons on the face of the twister,
-        also used to send out color values
-        """
-        self._param_buttons = buttons
-        self._param_buttons_changed.replace_subjects(buttons or [])
 
     def update(self):
         """Called by framework when the component needs to update"""
-        DeviceComponent.update(self)
+        TwisterDevice.update(self)
         if not self.is_enabled(): return
 
         self._on_on_off_change.subject = self._on_off_parameter()
@@ -37,7 +28,7 @@ class Twister16ParamDevice(DeviceComponent):
     def set_device(self, device):
         """Make sure to reset offset when device changes"""
         self.active_bank = 0
-        DeviceComponent.set_device(self, device)
+        TwisterDevice.set_device(self, device)
 
     @subject_slot('value')
     def _on_on_off_change(self):
@@ -80,31 +71,6 @@ class Twister16ParamDevice(DeviceComponent):
         colors[8 + self.active_bank] = BANK_ACTIVE_COLOR
         self.send_colors(colors)
 
-    def _bank_count(self):
-        """Total number of banks of 16 params"""
-        return int(ceil(len(self._get_all_params()) / 16.0))
-
-    def _get_all_params(self):
-        """All DeviceParameter that is a combo of the 'best of' 8 knobs + the rest"""
-        best_of = self._best_of_parameter_bank()
-        banks = self._parameter_banks()
-        bank_names = self._parameter_bank_names()
-
-        params = []
-
-        # if we have a best-of, use that as first 8 params always
-        if best_of: params.extend(best_of)
-
-        # if we dont have a best of (VST, etc) or we have more than one bank,
-        # add the rest
-        if not best_of or len(banks) > 1:
-            for name, bank in zip(bank_names, banks):
-
-                # dont add in macros since it will always be best-of
-                if name != 'Macros': params.extend([b for b in bank if b != None])
-
-        return params
-
     @subject_slot_group('value')
     def _param_buttons_changed(self, value, button):
         """Fired when a button was pressed"""
@@ -119,12 +85,4 @@ class Twister16ParamDevice(DeviceComponent):
             if off < self._bank_count():
                 self.active_bank = off
                 self._assign_parameters()
-
-    def send_colors(self, colors):
-        """Send out colors to the Twister, None will NOP and 0 will blank"""
-        for n, button in enumerate(self._param_buttons or []):
-            if not button: continue
-            if len(colors) <= n: continue
-            if colors[n] == None: continue
-            button.send_value(colors[n], force = True)
 
--- bvalosek_mf_twister/TwisterDevice.py
+++ bvalosek_mf_twister/TwisterDevice.py
@@ -1,4 +1,5 @@
 from _Framework.DeviceComponent import DeviceComponent
+from math import ceil
 
 class TwisterDevice(DeviceComponent):
     """Base device used for the various modules on the wister"""
@@ -21,4 +22,29 @@ class TwisterDevice(DeviceComponent):
             if len(colors) <= n: continue
             if colors[n] == None: continue
             button.send_value(colors[n], force = True)
+
+    def _bank_count(self):
+        """Total number of banks of 16 params"""
+        return int(ceil(len(self._get_all_params()) / 16.0))
+
+    def _get_all_params(self):
+        """All DeviceParameter that is a combo of the 'best of' 8 knobs + the rest"""
+        best_of = self._best_of_parameter_bank()
+        banks = self._parameter_banks()
+        bank_names = self._parameter_bank_names()
+
+        params = []
+
+        # if we have a best-of, use that as first 8 params always
+        if best_of: params.extend(best_of)
+
+        # if we dont have a best of (VST, etc) or we have more than one bank,
+        # add the rest
+        if not best_of or len(banks) > 1:
+            for name, bank in zip(bank_names, banks):
+
+                # dont add in macros since it will always be best-of
+                if name != 'Macros': params.extend([b for b in bank if b != None])
+
+        return params
 
