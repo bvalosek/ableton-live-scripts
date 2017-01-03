88f94bd3062b375824052e7f9e9e53c51e1b6bb1
Brandon Valosek <bvalosek@gmail.com>
2017-01-02 21:10:27 -0600

    comments and device switching

--- bvalosek_Alesis_VI_49/AlesisVI49ControlSurface.py
+++ bvalosek_Alesis_VI_49/AlesisVI49ControlSurface.py
@@ -10,12 +10,13 @@ from _Framework.SubjectSlot import subject_slot
 from consts import *
 
 class AlesisVI49ControlSurface(ControlSurface):
+    """
+    Simple control script for the Alesis VI 49
+    """
 
     def __init__(self, c_instance):
         ControlSurface.__init__(self, c_instance)
-
         self._handle_track_change.subject = self.song().view
-
         with self.component_guard():
             self._setup_controls()
             self._setup_device()
@@ -24,10 +25,16 @@ class AlesisVI49ControlSurface(ControlSurface):
 
     @subject_slot('selected_track')
     def _handle_track_change(self):
+        """Fired whenever the selected track has changed"""
         track = self.song().view.selected_track
         self._strip.set_track(track)
 
+        # always select the first device when selecting a new track
+        if track and len(track.devices):
+            self.song().view.select_device(track.devices[0])
+
     def _setup_controls(self):
+        """Create the element instances for the physical controls on the controller"""
         self._knobs = []
         for knob_index in range(KNOB_COUNT):
             self._knobs.append(SliderElement(
@@ -36,12 +43,14 @@ class AlesisVI49ControlSurface(ControlSurface):
                 identifier = KNOB_CC_START + knob_index))
 
     def _setup_device(self):
+        """Create a DeviceComponent to control macro knobs"""
         self._device = DeviceComponent()
         self.set_device_component(self._device)
         param_knobs = ButtonMatrixElement(rows = [self._knobs[0:8]])
         self._device.layer = Layer(parameter_controls = param_knobs)
 
     def _setup_strip(self):
+        """Create a ChannelStrip to control the sends"""
         self._strip = ChannelStripComponent()
         send_knobs = ButtonMatrixElement(rows = [self._knobs[8:]])
         self._strip.layer = Layer(send_controls = send_knobs)
