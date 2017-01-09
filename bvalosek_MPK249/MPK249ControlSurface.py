b58ccdd5d0e43b75d2163f32bc6d7cb27b734020
Brandon Valosek <bvalosek@gmail.com>
2017-01-08 18:34:47 -0600

    MPK249 stub

---
+++ bvalosek_MPK249/MPK249ControlSurface.py
@@ -0,0 +1,4 @@
+from _Framework.ControlSurface import ControlSurface
+
+class MPK249ControlSurface(ControlSurface):
+    pass
---
+++ bvalosek_MPK249/__init__.py
@@ -0,0 +1,4 @@
+from MPK249ControlSurface import MPK249ControlSurface
+
+def create_instance(c_instance):
+    return MPK249ControlSurface(c_instance)
