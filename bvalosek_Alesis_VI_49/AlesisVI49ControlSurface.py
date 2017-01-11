bca424dfef1cf4d6294ae0a6ac97cce5797f05bb
Brandon Valosek <bvalosek@gmail.com>
2017-01-10 23:57:44 -0600

    transport controls for alesis vi 49

--- bvalosek_Alesis_VI_49/AlesisVI49ControlSurface.py
+++ bvalosek_Alesis_VI_49/AlesisVI49ControlSurface.py
@@ -1,13 +1,18 @@
-from _Framework.ControlSurface import ControlSurface
-from _Framework.SliderElement import SliderElement
-from _Framework.InputControlElement import MIDI_CC_TYPE
-from _Framework.DeviceComponent import DeviceComponent
+from _Framework.ButtonElement import ButtonElement
 from _Framework.ButtonMatrixElement import ButtonMatrixElement
 from _Framework.ChannelStripComponent import ChannelStripComponent
+from _Framework.ControlSurface import ControlSurface
+from _Framework.DeviceComponent import DeviceComponent
+from _Framework.InputControlElement import MIDI_CC_TYPE
 from _Framework.Layer import Layer
+from _Framework.SliderElement import SliderElement
 from _Framework.SubjectSlot import subject_slot
+from _Framework.TransportComponent import TransportComponent
 
 from consts import *
+
+def create_button(channel, cc):
+    return ButtonElement(True, MIDI_CC_TYPE, channel, cc)
 
 class AlesisVI49ControlSurface(ControlSurface):
     """
@@ -21,11 +26,13 @@ class AlesisVI49ControlSurface(ControlSurface):
             self._setup_controls()
             self._setup_device()
             self._setup_strip()
+            self._setup_transport()
             self._handle_track_change()
 
     @subject_slot('selected_track')
     def _handle_track_change(self):
         """Fired whenever the selected track has changed"""
+
         track = self.song().view.selected_track
         self._strip.set_track(track)
 
@@ -35,15 +42,34 @@ class AlesisVI49ControlSurface(ControlSurface):
 
     def _setup_controls(self):
         """Create the element instances for the physical controls on the controller"""
-        self._knobs = []
-        for knob_index in range(KNOB_COUNT):
-            self._knobs.append(SliderElement(
+
+        self._knobs = [
+            SliderElement(
                 msg_type = MIDI_CC_TYPE,
                 channel = KNOB_CHANNEL,
-                identifier = KNOB_CC_START + knob_index))
+                identifier = KNOB_CC_START + knob_index)
+            for knob_index in range(KNOB_COUNT) ]
+
+        self._record_button = create_button(TRANSPORT_CHANNEL, TRANSPORT_RECORD)
+        self._loop_button = create_button(TRANSPORT_CHANNEL, TRANSPORT_LOOP)
+        self._backward_button = create_button(TRANSPORT_CHANNEL, TRANSPORT_BACKWARD)
+        self._forward_button = create_button(TRANSPORT_CHANNEL, TRANSPORT_FORWARD)
+        self._stop_button = create_button(TRANSPORT_CHANNEL, TRANSPORT_STOP)
+        self._play_button = create_button(TRANSPORT_CHANNEL, TRANSPORT_PLAY)
+
+    def _setup_transport(self):
+        self._transport = TransportComponent()
+        self._transport.layer = Layer(
+            play_button = self._play_button,
+            stop_button = self._stop_button,
+            loop_button = self._loop_button,
+            record_button = self._record_button,
+            seek_forward_button = self._forward_button,
+            seek_backward_button = self._backward_button)
 
     def _setup_device(self):
         """Create a DeviceComponent to control macro knobs"""
+
         self._device = DeviceComponent()
         self.set_device_component(self._device)
         param_knobs = ButtonMatrixElement(rows = [self._knobs[0:8]])
@@ -51,6 +77,8 @@ class AlesisVI49ControlSurface(ControlSurface):
 
     def _setup_strip(self):
         """Create a ChannelStrip to control the sends"""
+
         self._strip = ChannelStripComponent()
         send_knobs = ButtonMatrixElement(rows = [self._knobs[8:]])
         self._strip.layer = Layer(send_controls = send_knobs)
+
--- bvalosek_Alesis_VI_49/consts.py
+++ bvalosek_Alesis_VI_49/consts.py
@@ -1,8 +1,18 @@
-# 0-based MIDI channel the controller knobs are on
+# 0-based MIDI channels
 KNOB_CHANNEL = 0
+TRANSPORT_CHANNEL = 0
 
 # Starting CC of the knobs, assuming incrementing by 1 for each knob
 KNOB_CC_START = 20
 
 # Number of physical knobs on the device
 KNOB_COUNT = 12
+
+# Transport control CC
+TRANSPORT_RECORD = 114
+TRANSPORT_LOOP = 115
+TRANSPORT_BACKWARD = 116
+TRANSPORT_FORWARD = 117
+TRANSPORT_STOP = 118
+TRANSPORT_PLAY = 119
+
