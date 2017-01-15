57763c73be9db6d707e6b63efa42c46ea6676af4
Brandon Valosek <bvalosek@gmail.com>
2017-01-15 11:39:19 -0600

    back out of return track

--- bvalosek_MF_Twister/MixerComponentEx.py
+++ bvalosek_MF_Twister/MixerComponentEx.py
@@ -6,8 +6,13 @@ from itertools import izip_longest
 
 class ChannelStripComponentEx(ChannelStripComponentExBase):
     """
-    Special ChannelStrip for the twister that allows for skinning
+    Special ChannelStrip for the twister that allows for skinning, and skipping
+    back to a non-return track when re-selecting a return
     """
+
+    def __init__(self, mixer = None, *a, **k):
+        super(ChannelStripComponentEx, self).__init__(*a, **k)
+        self._mixer = mixer
 
     def set_arm_button(self, button):
         if button:
@@ -22,14 +27,33 @@ class ChannelStripComponentEx(ChannelStripComponentExBase):
                 self._select_button.set_on_off_values('Mixer.TrackSelected', 'Mixer.NoTrack')
         super(ChannelStripComponentEx, self).update()
 
+    def _select_value(self, value):
+        if value and self.song().view.selected_track == self._track:
+            self._mixer.back_to_last_non_return_track()
+            return
+        super(ChannelStripComponentEx, self)._select_value(value)
+
 class MixerComponentEx(MixerComponent):
     """
-    Special MixerComponent that allows for skinning
+    Special MixerComponent that allows for skinning and "return backing"
     """
+
+    def __init__(self, *a, **k):
+        self._last_track = None
+        super(MixerComponentEx, self).__init__(*a, **k)
 
     def set_return_track_select_buttons(self, buttons):
         for strip, button in izip_longest(self._return_strips, buttons or []):
             strip.set_select_button(button)
 
+    def back_to_last_non_return_track(self):
+        if self._last_track:
+            self.song().view.selected_track = self._last_track
+
+    def on_selected_track_changed(self):
+        track = self.song().view.selected_track
+        if track not in self.song().return_tracks:
+            self._last_track = track
+
     def _create_strip(self):
-        return ChannelStripComponentEx()
+        return ChannelStripComponentEx(mixer = self)
